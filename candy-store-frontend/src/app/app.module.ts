a8cd35537f2fc374b86872378a6e39737b3e0409
BozidarM <bozidar.mladenovic1@gmail.com>
2021-06-08 00:41:03 +0200

    Upload Files

---
+++ candy-store-frontend/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { RoutingModule } from './routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { UsersComponent } from './users/users.component';
+import { ProfileComponent } from './users/profile/profile.component';
+import { OrdersComponent } from './users/orders/orders.component';
+import { OrdersHistoryComponent } from './users/orders-history/orders-history.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    WelcomeComponent,
+    ProductsComponent,
+    CartComponent,
+    UsersComponent,
+    ProfileComponent,
+    OrdersComponent,
+    OrdersHistoryComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    RoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
